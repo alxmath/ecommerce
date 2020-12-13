69c30a645ddf9e732b0eb9ff040be10fb7a7d312
Alex Santos <alxmath@gmail.com>
2020-12-13 17:29:26 -0300

    carrinho calculo de frete

--- site.php
+++ site.php
@@ -60,6 +60,7 @@ $app->get('/cart', function () {
     $page->setTpl('cart', [
         'cart' => $cart->getValues(),
         'products' => $cart->getProducts(),
+        'error' => Cart::getMsgError(),
     ]);
 });
 
@@ -99,6 +100,14 @@ $app->get('/cart/:idproduct/remove', function ($idproduct) {
     $cart = Cart::getFromSession();
 
     $cart->removeProduct($product, true);
+    header('Location: /cart');
+
+    exit;
+});
+
+$app->post('/cart/freight', function () {
+    $cart = Cart::getFromSession();
+    $cart->setFreight($_POST['zipcode']);
     header('Location: /cart');
 
     exit;
