eff8923151d382b89afa6c8c589d15cff5724e21
Alex Santos <alxmath@gmail.com>
2020-12-13 21:37:42 -0300

    esqueceu a senha - site

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -148,7 +148,7 @@ class User extends Model
         ]);
     }
 
-    public static function getForgot($email)
+    public static function getForgot($email, $inadmin = true)
     {
         $sql = new Sql();
 
@@ -181,7 +181,12 @@ class User extends Model
             User::SECRET_IV
         ));
 
-        $link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code={$code}";
+        if ($inadmin) {
+            $link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code={$code}";
+        } else {
+            $link = "http://www.hcodecommerce.com.br/forgot/reset?code={$code}";
+        }
+
         $mailer = new Mailer($data['desemail'], $data['desperson'], 'Redefinir senha da Hcode Store', 'forgot', [
             'name' => $data['desperson'],
             'link' => $link,
